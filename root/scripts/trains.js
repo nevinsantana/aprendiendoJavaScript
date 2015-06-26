d747664b56c18b8317e08b82df340a96ddd4ae03
nevinsantana <roll.93@hotmail.com>
2015-06-26 15:09:19 -0500

    Se acabo lo gratis :cry:

---
+++ root/scripts/trains.js
@@ -0,0 +1,8 @@
+console.log("There #" + 1 + " is running.");
+console.log("There #" + 2 + " is running.");
+console.log("There #" + 3 + " is running.");
+console.log("There #" + 4 + " is running.");
+console.log("There #" + 5 + " is running.");
+console.log("There #" + 6 + " is running.");
+console.log("There #" + 7 + " is running.");
+console.log("There #" + 8 + " is running.");
